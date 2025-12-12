cd8280351afc4e263ed9e8c33bd649001e69a212
TheSkiF4er <cajeerskif4er@gmail.com>
2025-12-13 02:26:19 +0300

    Add files via upload

--- apps/api/src/modules/app.module.ts
+++ apps/api/src/modules/app.module.ts
@@ -1,1 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HealthController } from './health.controller';
 
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true
+    })
+  ],
+  controllers: [HealthController]
+})
+export class AppModule {}
